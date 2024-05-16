343afd8c24f10367aec6ddf02e0f8f232adedea4
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-16 21:56:34 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jack LeVasseur
+// Created on: April 23 2024
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function myButtonClicked() {
+  // input
+  const userNumber = parseInt(document.getElementById("user-number").value)
+
+  // process
+  if (userNumber == randomNumber) {
+    // output
+    document.getElementById("answer").innerHTML = "You have guesed the correct number!"
+  }
+
+  if (userNumber != randomNumber) {
+    document.getElementById("answer").innerHTML = "You have not guesed the correst number!"
+  }
+}
